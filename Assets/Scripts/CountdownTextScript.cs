c1bead266d3cda9a30a836224528b86bc04cb1fc
Charrière Loïc <loic.charriere@students.bfh.ch>
2019-03-26 17:54:22 +0100

    added countdown and updated Unity version

---
+++ Assets/Scripts/CountdownTextScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class CountdownTextScript : MonoBehaviour
+{
+    private DateTime mWeddingDate;
+    private Text mCountdownText;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        mWeddingDate = new DateTime(2019, 10, 11, 16, 0, 0);
+        mCountdownText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        TimeSpan countdown = mWeddingDate.Subtract(DateTime.Now);
+        mCountdownText.text = stringBuilder(countdown);
+    }
+
+    private string stringBuilder(TimeSpan duration)
+    {
+        string value = "";
+        value += duration.Days + " j : ";
+        value += duration.Hours + " h : ";
+        value += duration.Minutes + " m : ";
+        value += duration.Seconds + " s";
+        return value;
+    }
+}
